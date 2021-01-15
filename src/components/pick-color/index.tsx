c4dc00e68d2917fae3c36d9d3ce688264d07eb8b
Sun Yifeng <634709523@qq.com>
2021-01-15 11:06:07 +0800

    实现颜色选择器组件

---
+++ src/components/pick-color/index.tsx
@@ -0,0 +1,101 @@
+import React, { memo, useState, useCallback, useMemo } from 'react';
+import {
+  SketchPicker,
+  GithubPicker,
+  TwitterPicker,
+  ChromePicker,
+  ColorResult
+} from 'react-color';
+import { Popover } from 'antd';
+import './index.less';
+/**
+ * 颜色选择器
+ */
+
+interface IProps {
+  onChangeComplete: (color: string) => void;
+  type?: string;
+  position?: string;
+  themeColor?: string;
+}
+
+const pickers: {
+  [key: string]: React.ReactNode;
+} = {
+  chrome: ChromePicker,
+  github: GithubPicker,
+  twitter: TwitterPicker,
+  sketch: SketchPicker
+};
+
+const PickColor: React.FC<IProps> = props => {
+  const {
+    type = 'sketch',
+    position = 'bottom',
+    themeColor = '#555555',
+    onChangeComplete
+  } = props;
+  const [displayColorPicker, setDisplayColorPicker] = useState(false);
+  const [color, setColor] = useState(themeColor);
+  const Picker: any = pickers[type] ? pickers[type] : SketchPicker;
+
+  const handleChangeComplete = (color: any) => {
+    onChangeComplete(color.hex);
+    setColor(color.hex);
+  };
+  // 展示色块的点击
+  const handleDisplayColorPicker = useCallback(() => {
+    setDisplayColorPicker(true);
+  }, [displayColorPicker]);
+  // 关闭颜色选择器
+  const handleClosePicker = useCallback(() => {
+    setDisplayColorPicker(false);
+  }, [displayColorPicker]);
+  const { swatch, picker } = useMemo(() => {
+    const styles: any = {
+      wrapper: {
+        position: 'inherit',
+        zIndex: 100
+      }
+    };
+    if (position === 'top') {
+      styles.wrapper.transform = 'translateY(-100%)';
+      styles.wrapper.paddingBottom = 0;
+    }
+    const swatch = (
+      <Popover content="更改主题色">
+        <div className="swatch" onClick={handleDisplayColorPicker}>
+          <div
+            className="swatch-color"
+            style={{
+              background: color
+            }}
+          />
+        </div>
+      </Popover>
+    );
+    const picker = displayColorPicker ? (
+      <div className="popover">
+        <div className="cover" onClick={handleClosePicker} />
+        <div style={styles.wrapper}>
+          <Picker
+            color={color}
+            onChangeComplete={handleChangeComplete}
+          />
+        </div>
+      </div>
+    ) : null;
+    return {
+      swatch,
+      picker
+    };
+  }, [displayColorPicker, position, color]);
+  return (
+    <div className="pick-color">
+      {swatch}
+      {picker}
+    </div>
+  );
+};
+
+export default memo(PickColor);
--- src/layout/components/rightmenu/index.tsx
+++ src/layout/components/rightmenu/index.tsx
@@ -1,25 +1,23 @@
 /**
  * 顶部右侧设置组件
  */
-import React, { memo } from 'react'
-import PickColor from '../../../components/pick-color'
+import React, { memo, useCallback } from 'react';
+import PickColor from '../../../components/pick-color';
 import './index.less';
-interface IProps {
- 
-}
+interface IProps {}
 
-const RightMenu: React.FC<IProps> = (props) => {
+const RightMenu: React.FC<IProps> = props => {
+  // 改变主题颜色事件
+  const handleColorChange = useCallback((color: string) => {}, []);
   return (
     <div className="right-menu">
       <div className="pick-color">
-        <span></span>
+        <PickColor onChangeComplete={handleColorChange} />
       </div>
-      <div className="language">
-        简体中文
-      </div>
+      <div className="language">简体中文</div>
       <div className="user">用户相关</div>
     </div>
-  )
-}
+  );
+};
 
-export default memo(RightMenu)
+export default memo(RightMenu);
